ff304d6a2d454dd68d05e392af38869fb58f4878
Atakan Ermiş <ataerms@gmail.com>
2019-12-14 14:48:59 +0300

    initial

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sözlük
+
+## Kullanım
+
+- Mesajlarınızda `@trsozlukbot <kelime>` şeklinde kullanabilirsiniz.
+
+## Kendim Host etmek istiyorum.
+
+- Repoyu klonlayın: `git clone https://github.com/AtakanErmis/SozlukBot`
+- Eğer makinenizde PM2 kurulu değilse kurun: `npm install -g pm2`
+- Bağımlılıkları kurun: `npm install`
+- .env dosyasını sample.env dosyasına göre düzenleyin. (BotFather üzerinden bir Telegram Bot hesabı açmanız gerekir)
+- PM2 Daemon'u başlatın: `pm2-runtime start pm2.json`
---
+++ index.js
@@ -0,0 +1,64 @@
+require("dotenv").config();
+
+const Telegraf = require("telegraf");
+const sozluk = require("sozlukjs");
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.on("inline_query", async ctx => {
+	try {
+		const meaningTDK = await sozluk.TDKDictionary.getMeaningData(
+			ctx.update.inline_query.query
+		);
+		const meaningTureng = await sozluk.TurengDictionary.getTermData(
+			ctx.update.inline_query.query
+		);
+		let result = [];
+		if (!meaningTDK.error) {
+				result = meaningTDK.map(e => {
+					return {
+						type: "article",
+						id: `${e.kelime_no}: ${e.madde_id}`,
+						title: "TDK: " + e.madde,
+						description:
+							e.anlamlarListe[0].anlam.substring(0, 50) + "...",
+						input_message_content: {
+							message_text:
+								`<b>${e.madde}</b>\n\n` +
+								e.anlamlarListe
+									.map(
+										(e, index) =>
+											`<b>${index + 1}.</b> ${e.anlam}`
+									)
+									.join("\n"),
+							parse_mode: "HTML"
+						}
+					};
+				})
+		}
+		if (!meaningTureng.Results) {
+			result.push({
+				type: "article",
+				id: meaningTureng.MobileResult.Term,
+				title: "Tureng: " + meaningTureng.MobileResult.Term,
+				description: meaningTureng.MobileResult.Results[0].Term,
+				input_message_content: {
+					message_text:
+						`<b>${meaningTureng.MobileResult.Term}</b>\n\n` +
+						meaningTureng.MobileResult.Results.map(
+							(e, index) =>
+								`<b>${index + 1}.</b> <i>${e.TypeEN ? e.TypeEN : ""} ${
+									e.CategoryTR ? e.CategoryTR : ""
+								}</i>: ${e.Term}`
+						).join("\n"),
+					parse_mode: "HTML"
+				}
+			});
+		}
+
+		ctx.answerInlineQuery(result);
+	} catch (err) {
+		console.error(err);
+    }
+});
+
+bot.launch();
